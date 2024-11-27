afd37ff8f46a2bb45f1df08cc068d1fcc4d40efd
parlakyan <narek@themediaworx.com>
2024-11-27 00:06:35 -0800

    Fix
    Fix

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -37,7 +37,7 @@ export default function HomePage() {
   }, []);
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 test">
       <Header />
       <HeroSection />
       <TreatmentFinder />
